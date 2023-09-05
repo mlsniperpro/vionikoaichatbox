1a92cb928516a27ee649cfe0be975884dd47005e
mlsniperpro <algopro.km@gmail.com>
2023-09-06 01:46:28 +0300

    Higher zIndex

--- client/chatWidget.js
+++ client/chatWidget.js
@@ -16,36 +16,33 @@ const loadStyles = () => {
 const generateRandomId = () => {
   const characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < 17; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
+  return Array.from(
+    { length: 17 },
+    () => characters[Math.floor(Math.random() * characters.length)]
+  ).join("");
 };
 
 // Generate form fields based on window.vionikoaiChat properties
 const generateFormFields = () => {
-  const fields = [];
   const chatProps = window.vionikoaiChat || {};
-  ["name", "email", "phone"].forEach((field) => {
-    if (chatProps[field]) {
-      fields.push(
-        `<label for="${field}">${
-          field.charAt(0).toUpperCase() + field.slice(1)
-        }:</label><input type="${
-          field === "email" ? "email" : "text"
-        }" id="${field}" name="${field}" required>`
-      );
-    }
-  });
-  return fields.join("");
+  return ["name", "email", "phone"]
+    .map((field) =>
+      chatProps[field]
+        ? `<label for="${field}">${
+            field.charAt(0).toUpperCase() + field.slice(1)
+          }:</label><input type="${
+            field === "email" ? "email" : "text"
+          }" id="${field}" name="${field}" required>`
+        : ""
+    )
+    .join("");
 };
 
 // Append form HTML to the chat
 const appendFormHTML = () => {
   const formFields = generateFormFields();
   if (formFields) {
-    const formHTML = `<div id="form-overlay" class="form-overlay"><form id="user-form">${formFields}<input type="submit" value="Submit"></form></div>`;
+    const formHTML = `<div id="form-overlay" class="form-overlay" style="z-index: 9999;"><form id="user-form">${formFields}<input type="submit" value="Submit"></form></div>`;
     document
       .querySelector(".outer-container")
       .insertAdjacentHTML("beforeend", formHTML);
@@ -69,10 +66,9 @@ const validateForm = () => {
   const email = document.getElementById("email")?.value;
   const phone = document.getElementById("phone")?.value;
   if (name && email && phone) {
-    const chatId = generateRandomId();
     window.vionikoaiChat = {
       ...window.vionikoaiChat,
-      chatId,
+      chatId: generateRandomId(),
       name,
       email,
       phone,
@@ -92,34 +88,35 @@ const appendChatHTML = () => {
   const inputPlaceholder =
     window.vionikoaiChat?.inputPlaceholder || "Tap Enter to send a message";
   const chatName = window.vionikoaiChat?.chatName || "VionikoAIChat!";
-  const chatHTML = `<div class="chat-bar-collapsible"><button id="chat-button" type="button" class="collapsible chat-button" aria-label="Open chat">${chatName}<i class="fa fa-fw fa-comments-o chat-icon"></i></button><div class="content chat-content"><div class="full-chat-block"><div class="outer-container"><div class="chat-container"><div id="chatbox" class="chatbox"><h5 id="chat-timestamp" class="chat-timestamp"></h5><p id="botStarterMessage" class="botText chat-bot-message"><span>Loading...</span></p></div><div class="chat-bar-input-block"><div id="userInput" class="user-input"><input id="textInput" class="input-box chat-input-box" type="text" name="msg" placeholder="${inputPlaceholder}" /></div></div><div id="chat-bar-bottom"></div></div></div></div></div>`;
+  const chatHTML = `<div class="chat-bar-collapsible" style="z-index: 2000000001;"><button id="chat-button" type="button" class="collapsible chat-button" aria-label="Open chat">${chatName}<i class="fa fa-fw fa-comments-o chat-icon"></i></button><div class="content chat-content"><div class="full-chat-block"><div class="outer-container"><div class="chat-container"><div id="chatbox" class="chatbox"><h5 id="chat-timestamp" class="chat-timestamp"></h5><p id="botStarterMessage" class="botText chat-bot-message"><span>Loading...</span></p></div><div class="chat-bar-input-block"><div id="userInput" class="user-input"><input id="textInput" class="input-box chat-input-box" type="text" name="msg" placeholder="${inputPlaceholder}" /></div></div><div id="chat-bar-bottom"></div></div></div></div></div>`;
   document.body.insertAdjacentHTML("beforeend", chatHTML);
 };
 
 // Load chat script
 const loadChatScript = () => {
   const chatScript = document.createElement("script");
-  chatScript.src = "https://mlsniperpro.github.io/vionikoaichatbox/client/static/scripts/chat.js";
-  chatScript.async = true; // Make it non-blocking
+  chatScript.src =
+    "https://mlsniperpro.github.io/vionikoaichatbox/client/static/scripts/chat.js";
+  chatScript.async = true;
   document.body.appendChild(chatScript);
 };
 
 // Load required scripts
 const loadScripts = () => {
   const scripts = ["https://cdn.jsdelivr.net/npm/marked/marked.min.js"];
-  let loadedScripts = 0;
-  scripts.forEach((src) => {
-    const script = document.createElement("script");
-    script.src = src;
-    script.async = true; // Make it non-blocking
-    script.onload = () => {
-      loadedScripts++;
-      if (loadedScripts === scripts.length) {
-        loadChatScript();
-        initializeForm();
-      }
-    };
-    document.head.appendChild(script);
+  Promise.all(
+    scripts.map((src) => {
+      return new Promise((resolve) => {
+        const script = document.createElement("script");
+        script.src = src;
+        script.async = true;
+        script.onload = resolve;
+        document.head.appendChild(script);
+      });
+    })
+  ).then(() => {
+    loadChatScript();
+    initializeForm();
   });
 };
 
