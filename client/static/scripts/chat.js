a23e5adfa2c9c9d845f004977686926f78a53075
mlsniperpro <algopro.km@gmail.com>
2024-02-10 07:59:55 +0300

    Adding the new model

--- client/static/scripts/chat.js
+++ client/static/scripts/chat.js
@@ -206,7 +206,7 @@ const fetchResponse = async (chat, userId) => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "gpt-3.5-turbo-1106",
+        model: "gpt-3.5-turbo-0125",
         messages: chat,
         stream: true,
       }),
